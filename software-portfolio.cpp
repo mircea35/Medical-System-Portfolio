283f0ca169a589143ae019e61b040cd518c80cb7
Petpet <42213952+mircea-ionescu78@users.noreply.github.com>
2023-04-13 00:34:23 +0100

    Update software-portfolio.cpp

--- software-portfolio.cpp
+++ software-portfolio.cpp
@@ -4,11 +4,9 @@
 #include <unordered_map>
 #include <string>
 
-using namespace std;
-
-vector<string> user_info;
+std::vector<std::string> user_info;
 bool main_loop = true, loop_two = true;
-string temporary, username, password, name = "";
+std::string temporary, username, password, name = "";
 size_t saved_hash;
 int is_admin, user_option, age, cancer, cancer_level, is_smoker, smoking_quantity, cancer_history, smoking_history, diabetes_type = 0;
 
@@ -16,11 +14,11 @@ int is_admin, user_option, age, cancer, cancer_level, is_smoker, smoking_quantit
 
 void login() {
 
-    cout << "Login as:" << endl;
-    cout << "1 - Patient" << endl;
-    cout << "2 - Medical Professional" << endl;
+    std::cout << "Login as:" << std::endl;
+    std::cout << "1 - Patient" << std::endl;
+    std::cout << "2 - Medical Professional" << std::endl;
 
-    cin >> user_option;
+    std::cin >> user_option;
 
     switch (user_option) {
         case 1:
@@ -31,7 +29,7 @@ void login() {
             break;
 
         default:
-            cout << "Error - Try again" << endl;
+            std::cout << "Error - Try again" << std::endl;
             break;
     }
 
@@ -41,22 +39,22 @@ void login() {
     std::cin >> password;
 
     if (is_admin == 1) {
-        ifstream admins("admins.csv");
+        std::ifstream admins("admins.csv");
         while (getline(admins, temporary, ',')) {
             user_info.push_back(temporary);
         }
     }
     else {
-        ifstream users("users.csv");
+        std::ifstream users("users.csv");
         while (getline(users, temporary, ',')) {
             user_info.push_back(temporary);
         }
     }
 
-    std::hash<string> hasher;
+    std::hash<std::string> hasher;
     size_t hash = hasher(password);
     if (hash == saved_hash) {
-        cout << "Right Password";
+        std::cout << "Right Password";
         switch (is_admin) {
             case 0:
 
@@ -65,28 +63,28 @@ void login() {
                 break;
 
             default:
-                cout << "Error - Try again" << endl;
+                std::cout << "Error - Try again" << std::endl;
                 break;
         }
     }
     else {
-        cout << "Wrong Password" << endl;
+        std::cout << "Wrong Password" << std::endl;
     }
 }
 
 void smoking_status() {
 
-    std::cout << "Are you smoking?" << endl;
-    std::cout << "1 - Yes" << endl;
-    std::cout << "2 - No" << endl;
+    std::cout << "Are you smoking?" << std::endl;
+    std::cout << "1 - Yes" << std::endl;
+    std::cout << "2 - No" << std::endl;
     std::cin >> user_option;
     std::cin >> is_smoker;
     switch (is_smoker) {
     case 1:
-        std::cout << "How much are you smoking?" << endl;
-        std::cout << "1 - One pack per month" << endl;
-        std::cout << "2 - One pack per week" << endl;
-        std::cout << "3 - One pack per day" << endl;
+        std::cout << "How much are you smoking?" << std::endl;
+        std::cout << "1 - One pack per month" << std::endl;
+        std::cout << "2 - One pack per week" << std::endl;
+        std::cout << "3 - One pack per day" << std::endl;
         std::cin >> smoking_quantity;
         break;
 
@@ -103,13 +101,17 @@ void smoking_status() {
 
 
 void register_user() {
-    std::cout << "Registering new user..." << endl;
+    std::cout << "Registering new user..." << std::endl;
 
     std::cout << "Username: ";
     std::cin >> username;
+    std::cin.sync();
+    std::cin.get();
 
     std::cout << "Full Name: ";
-    std::cin >> name;
+    std::getline(std::cin, name, '\n');
+
+    std::cout << name << username;
 
     std::cout << "Age: ";
     std::cin >> age;
@@ -117,9 +119,9 @@ void register_user() {
     std::cout << "Set password: ";
     std::cin >> password;
 
-    std::cout << "Do you have Lung Cancer?" << endl;
-    std::cout << "1 - Yes" << endl;
-    std::cout << "2 - No" << endl;
+    std::cout << "Do you have Lung Cancer?" << std::endl;
+    std::cout << "1 - Yes" << std::endl;
+    std::cout << "2 - No" << std::endl;
     std::cin >> user_option;
 
     switch(user_option){
@@ -127,49 +129,49 @@ void register_user() {
             user_option = 0;
             main_loop = false;
             cancer_level = 1;
-                std::cout << "What is the stage of the cancer?" << endl;
+                std::cout << "What is the stage of the cancer?" << std::endl;
                 std::cin >> cancer_level;
                 if (cancer_level > 4) {
-                    cout << "The stage of the cancer cannot be bigger than 4. Please try again." << endl;
+                    std::cout << "The stage of the cancer cannot be bigger than 4. Please try again." << std::endl;
                 }
             break;
 
         case 2:
             cancer_level = 0;
-            std::cout << "Thanks for letting us know!" << endl;
+            std::cout << "Thanks for letting us know!" << std::endl;
             break;
         
         default:
-            cout << "Error - Incorrect answer" << endl;
+            std::cout << "Error - Incorrect answer" << std::endl;
             break;
     }
 
-    std::cout << "Do you have any type of diabetes?" << endl;
-    std::cout << "1 - Yes" << endl;
-    std::cout << "2 - No" << endl;
+    std::cout << "Do you have any type of diabetes?" << std::endl;
+    std::cout << "1 - Yes" << std::endl;
+    std::cout << "2 - No" << std::endl;
     std::cin >> user_option;
     switch (user_option) {
         case 1:
-            std::cout << "Which type of diabetes you have?" << endl;
-            std::cout << "1 - Type 1" << endl;
-            std::cout << "2 - Type 2" << endl;
+            std::cout << "Which type of diabetes you have?" << std::endl;
+            std::cout << "1 - Type 1" << std::endl;
+            std::cout << "2 - Type 2" << std::endl;
             std::cin >> diabetes_type;
             break;
         case 2:
             diabetes_type = 0;
             break;
         default:
-            std::cout << "Error: Incorrect answer" << endl;
+            std::cout << "Error: Incorrect answer" << std::endl;
             break;
     }
     
 
     smoking_status();
 
-    std::cout << "Now, let's ask some questions about your medical history." << endl;
-    std::cout << "Did you had Lung Cancer before?" << endl;
-    std::cout << "1 - Yes" << endl;
-    std::cout << "2 - No" << endl;
+    std::cout << "Now, let's ask some questions about your medical history." << std::endl;
+    std::cout << "Did you had Lung Cancer before?" << std::endl;
+    std::cout << "1 - Yes" << std::endl;
+    std::cout << "2 - No" << std::endl;
     std::cin >> user_option;
     switch (user_option) {
     case 1:
@@ -178,13 +180,13 @@ void register_user() {
         cancer_history = 0;
         break;
     default:
-        cout << "Error - Incorrect answer";
+        std::cout << "Error - Incorrect answer";
         break;
     }
 
-    std::cout << "Did you smoked before?" << endl;
-    std::cout << "1 - Yes" << endl;
-    std::cout << "2 - No" << endl;
+    std::cout << "Did you smoked before?" << std::endl;
+    std::cout << "1 - Yes" << std::endl;
+    std::cout << "2 - No" << std::endl;
     std::cin >> user_option;
     switch (user_option) {
     case 1:
@@ -193,16 +195,16 @@ void register_user() {
         smoking_history = 0;
         break;
     default:
-        cout << "Error - Incorrect answer" << endl;
+        std::cout << "Error - Incorrect answer" << std::endl;
         break;
     }
 
 
-    std::hash<string> hasher;
+    std::hash<std::string> hasher;
     size_t hash = hasher(password);
 
-    ofstream users("users.csv");
-    users << username << "," << hash << "," << name << "," << age << "," << cancer_level << "," << diabetes_type << "," << smoking_quantity << "," << cancer_history << "," << smoking_history;
+    std::ofstream users("users.csv");
+    users << username << "," << hash << "," << name << "," << age << "," << cancer_level << "," << diabetes_type << "," << smoking_quantity << "," << cancer_history << "," << smoking_history << std::endl;
     users.close();
 }
 
@@ -210,10 +212,10 @@ void register_user() {
 int main()
 {
     while (main_loop) {
-        std::cout << "Welcome to the NHS Command Line Program" << endl;
-        std::cout << "What do you want to do?" << endl;
-        std::cout << "1 - Register new patient" << endl;
-        std::cout << "2 - Login as a patient or medical professional" << endl;
+        std::cout << "Welcome to the NHS Command Line Program" << std::endl;
+        std::cout << "What do you want to do?" << std::endl;
+        std::cout << "1 - Register new patient" << std::endl;
+        std::cout << "2 - Login as a patient or medical professional" << std::endl;
         std::cin >> user_option;
 
         switch (user_option) {
@@ -225,7 +227,7 @@ int main()
             break;
 
         default:
-            std::cout << "Error - Try again" << endl;
+            std::cout << "Error - Try again" << std::endl;
             break;
         }
     }
